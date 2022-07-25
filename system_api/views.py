bcde2aa79d97fc54a7b9809fed5a94fb6d4a6362
DefinitelyNotAnAssassin <dharklike@gmail.com>
2022-07-25 08:02:58 +0800

    Fifth Commit

--- system_api/views.py
+++ system_api/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from django.utils.safestring import mark_safe
 # Create your views here.
 
 
 def get_table(request):
     data = {"message": "hello world!"}
+    data2 = mark_safe("<tr> Hello </tr>")
+    return HttpResponse(data2)
 
-    return JsonResponse(data)
